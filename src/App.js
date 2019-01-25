f247a45baff2a4d23584daf02c1a5adce9d2c06c
David Herr <davidherraz@outlook.com>
2019-01-24 21:56:09 -0700

    Add book component for re-use in list and search. Implement in ListBooks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import * as BooksAPI from './BooksAPI';
+import * as BooksAPI from './BooksAPI';
 import { Route } from 'react-router-dom';
 import ListBooks from './components/ListBooks';
 import SearchBooks from './components/SearchBooks';
@@ -10,10 +10,20 @@ class BooksApp extends React.Component {
     books: []
   };
 
+  componentDidMount() {
+    BooksAPI.getAll().then(books => {
+      this.setState({ books });
+    });
+  }
+
   render() {
     return (
       <div className="app">
-        <Route exact path="/" render={() => <ListBooks />} />
+        <Route
+          exact
+          path="/"
+          render={() => <ListBooks books={this.state.books} />}
+        />
         <Route path="/search" render={() => <SearchBooks />} />
       </div>
     );
---
+++ src/components/Book.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Book extends Component {
+  static propTypes = {
+    books: PropTypes.array.isRequired
+  };
+
+  render() {
+    const { books } = this.props;
+
+    return (
+      <ol className="books-grid">
+        {books.map(book => (
+          <li key={book.id}>
+            <div className="book">
+              <div className="book-top">
+                <div
+                  className="book-cover"
+                  style={{
+                    width: 128,
+                    height: 193,
+                    backgroundImage: `url(${book.imageLinks.smallThumbnail})`
+                  }}
+                />
+                <div className="book-shelf-changer">
+                  <select value={book.shelf || 'none'}>
+                    <option value="move" disabled>
+                      Move to...
+                    </option>
+                    <option value="currentlyReading">Currently Reading</option>
+                    <option value="wantToRead">Want to Read</option>
+                    <option value="read">Read</option>
+                    <option value="none">None</option>
+                  </select>
+                </div>
+              </div>
+              <p className="book-title">{book.title}</p>
+              <p className="book-authors">{book.authors}</p>
+            </div>
+          </li>
+        ))}
+      </ol>
+    );
+  }
+}
+
+export default Book;
