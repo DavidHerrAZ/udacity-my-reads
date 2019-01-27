92aaea8bbd26c8c8a61db83f03b8adb11a7a1371
David Herr <davidherraz@outlook.com>
2019-01-27 15:46:32 -0700

    Fix handling for no image link in Book

--- src/components/Book.js
+++ src/components/Book.js
@@ -21,7 +21,10 @@ class Book extends Component {
                   style={{
                     width: 128,
                     height: 193,
-                    backgroundImage: `url(${book.imageLinks.smallThumbnail})`
+                    backgroundImage: `url(${(book.imageLinks &&
+                      book.imageLinks.thumbnail) ||
+                      book.imageLinks.smallThumbnail ||
+                      ''})`
                   }}
                 />
                 <div className="book-shelf-changer">
